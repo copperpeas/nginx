e9e1187cd4072e58351a7aaff91da4911056927a
copperPeas <shilun1@staff.sina.com.cn>
2017-05-10 20:57:59 +0800

    add note

--- objs/ngx_modules.c
+++ objs/ngx_modules.c
@@ -4,6 +4,7 @@
 
 
 
+nginx模块的顺序很重要，会直接影响到程序的功能。而且，nginx和部分模块，也有着自己特定的顺序要求，比如ngx_http_write_filter_module模块一定要在filter模块的最后一步执行。想查看模块的执行顺序，可以在objs/ngx_modules.c这个文件中找到，这个文件在configure之后生成，上一节中，我们看过这个文件里面的内容
 extern ngx_module_t  ngx_core_module;
 extern ngx_module_t  ngx_errlog_module;
 extern ngx_module_t  ngx_conf_module;
@@ -54,17 +55,22 @@ extern ngx_module_t  ngx_http_range_body_filter_module;
 extern ngx_module_t  ngx_http_not_modified_filter_module;
 
 ngx_module_t *ngx_modules[] = {
+     // 全局core模块
     &ngx_core_module,
     &ngx_errlog_module,
     &ngx_conf_module,
+    // 正则模块
     &ngx_regex_module,
+    // event模块
     &ngx_events_module,
     &ngx_event_core_module,
     &ngx_epoll_module,
+     // http模块
     &ngx_http_module,
     &ngx_http_core_module,
     &ngx_http_log_module,
     &ngx_http_upstream_module,
+    // http handler模块
     &ngx_http_static_module,
     &ngx_http_autoindex_module,
     &ngx_http_index_module,
@@ -89,6 +95,8 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_upstream_least_conn_module,
     &ngx_http_upstream_keepalive_module,
     &ngx_http_upstream_zone_module,
+    //此处是第三方handler模块
+    // http filter模块
     &ngx_http_write_filter_module,
     &ngx_http_header_filter_module,
     &ngx_http_chunked_filter_module,
@@ -99,6 +107,7 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_charset_filter_module,
     &ngx_http_userid_filter_module,
     &ngx_http_headers_filter_module,
+    // 第三方filter模块
     &ngx_http_copy_filter_module,
     &ngx_http_range_body_filter_module,
     &ngx_http_not_modified_filter_module,
@@ -156,4 +165,10 @@ char *ngx_module_names[] = {
     "ngx_http_not_modified_filter_module",
     NULL
 };
+
+
+#phoenix add #
+#http handler模块与http filter模块的顺序很重要，这里我们主要关注一下这两类模块。
+#http handler模块，在后面的章节里面会讲到多阶段请求的处理链。对于content phase之前的handler，同一个阶段的handler，模块是顺序执行的。比如上面的示例代码中，ngx_http_auth_basic_module与ngx_http_access_module这两个模块都是在access phase阶段，由于ngx_http_auth_basic_module在前面，所以会先执行。由于content phase只会有一个执行，所以不存在顺序问题。另外，我们加载的第三方handler模块永远是在最后执行。
+#http filter模块，filter模块会将所有的filter handler排成一个倒序链，所以在最前面的最后执行。上面的例子中，&ngx_http_write_filter_module最后执行，ngx_http_not_modified_filter_module最先执行。注意，我们加载的第三方filter模块是在copy_filter模块之后，headers_filter模块之前执行。
 
